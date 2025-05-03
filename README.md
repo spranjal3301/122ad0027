2de622f2a5e023770553e469431ffde4708bbb59
Pranjal <spranjal394@gmail.com>
2025-05-03 20:13:00 +0530

    Done que2

---
+++ README.md
@@ -0,0 +1,25 @@
+# Frontend
+
+## QUE 1 :
+
+## Social Media Analytics:
+### Technologies Used:
+- Next.js
+- React.js
+- Tailwind CSS
+- Typescript
+- Material UI
+
+### Output:
+![Demo Model](https://github.com/spranjal3301/122ad0027/blob/main/que1_output/que1_output1.png?raw=true)
+
+## QUE 2 :
+
+## Avg Calculator:
+### Technologies Used:
+- Node.js
+- Express.js
+- axios
+
+### Output:
+![Demo Model](https://github.com/spranjal3301/122ad0027/blob/main/que2_output/que2_output1.png?raw=true)
---
+++ que2/index.js
@@ -0,0 +1,107 @@
+const express = require('express');
+const axios = require('axios');
+
+require('dotenv').config()
+
+const app = express();
+const PORT = 8080;
+const WINDOW_SIZE = 10;
+
+const windowList = [];
+const windowSet = new Set(); 
+
+const baseURL = process.env.API_BASE_URL;
+const ID_MAP = {
+    p: `${baseURL}/primes`,
+    f: `${baseURL}/fibo`,
+    e: `${baseURL}/even`,
+    r: `${baseURL}/rand`,
+};
+
+app.use(cors());
+
+
+
+const accessToken = process.env.ACCESS_TOKEN;
+
+const axiosClient = axios.create({
+  baseURL,
+  headers: {
+    Authorization: `Bearer ${accessToken}`,
+    'Content-Type': 'application/json',
+  },
+});
+
+
+
+const fetchWithTimeout = async (url, timeout = 500) => {
+  return new Promise((resolve, reject) => {
+    const timer = setTimeout(() => reject(new Error('Timeout')), timeout);
+
+    axiosClient.get(url)
+      .then(res => res.json())
+      .then(data => {
+        clearTimeout(timer);
+        resolve(data);
+      })
+      .catch(err => {
+        clearTimeout(timer);
+        reject(err);
+      });
+  });
+};
+
+app.get('/numbers/:id', async (req, res) => {
+  const id = req.params.id;
+  const url = ID_MAP[id];
+
+  if (!url) {
+    return res.status(400).json({ error: 'Invalid number ID' });
+  }
+
+  const windowPrevState = [...windowList];
+  let apiNumbers = [];
+
+  try {
+    const data = await fetchWithTimeout(url);
+    apiNumbers = data.numbers || [];
+  } catch (err) {
+    return res.json({
+      windowPrevState,
+      windowCurrState: windowList,
+      numbers: [],
+      avg: calcAverage(windowList)
+    });
+  }
+
+
+  const newNumbers = [];
+  for (const num of apiNumbers) {
+    if (!windowSet.has(num)) {
+      newNumbers.push(num);
+      windowSet.add(num);
+      windowList.push(num);
+      if (windowList.length > WINDOW_SIZE) {
+        const removed = windowList.shift();
+        windowSet.delete(removed);
+      }
+    }
+  }
+
+  return res.json({
+    windowPrevState,
+    windowCurrState: windowList,
+    numbers: apiNumbers,
+    avg: calcAverage(windowList)
+  });
+});
+
+function calcAverage(arr) {
+  if (arr.length === 0) return 0.0;
+  const sum = arr.reduce((a, b) => a + b, 0);
+  return parseFloat((sum / arr.length).toFixed(2));
+}
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
